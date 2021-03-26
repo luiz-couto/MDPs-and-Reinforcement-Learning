5e322c9e8ff850d88409e54ea219c6a3baa193aa
luiz-couto <luiz.felipe.556@outlook.com>
2021-03-26 19:31:20 -0300

    step 5

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -23,20 +23,20 @@ class QLearningAgent(ReinforcementAgent):
       Q-Learning Agent
 
       Functions you should fill in:
-        - computeValueFromQValues
-        - computeActionFromQValues
-        - getQValue
-        - getAction
-        - update
+      - computeValueFromQValues
+      - computeActionFromQValues
+      - getQValue
+      - getAction
+      - update
 
       Instance variables you have access to
-        - self.epsilon (exploration prob)
-        - self.alpha (learning rate)
-        - self.discount (discount rate)
+      - self.epsilon (exploration prob)
+      - self.alpha (learning rate)
+      - self.discount (discount rate)
 
       Functions you should use
-        - self.getLegalActions(state)
-          which returns legal actions for a state
+      - self.getLegalActions(state)
+        which returns legal actions for a state
     """
     def __init__(self, **args):
         """You can initialize Q-values here..."""
@@ -46,63 +46,85 @@ class QLearningAgent(ReinforcementAgent):
 
     def getQValue(self, state, action):
         """
-          Returns Q(state,action)
-          Should return 0.0 if we have never seen a state
-          or the Q node value otherwise
+            Returns Q(state,action)
+            Should return 0.0 if we have never seen a state
+            or the Q node value otherwise
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.qValues[(state, action)]
 
 
     def computeValueFromQValues(self, state):
         """
-          Returns max_action Q(state,action)
-          where the max is over legal actions.  Note that if
-          there are no legal actions, which is the case at the
-          terminal state, you should return a value of 0.0.
+            Returns max_action Q(state,action)
+            where the max is over legal actions.  Note that if
+            there are no legal actions, which is the case at the
+            terminal state, you should return a value of 0.0.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        actions = self.getLegalActions(state)
+        values = []
+
+        if len(actions) == 0:
+            return 0
+        
+        for action in actions:
+            values.append(self.getQValue(state, action))
+
+        return max(values)
+
+
 
     def computeActionFromQValues(self, state):
         """
-          Compute the best action to take in a state.  Note that if there
-          are no legal actions, which is the case at the terminal state,
-          you should return None.
+            Compute the best action to take in a state.  Note that if there
+            are no legal actions, which is the case at the terminal state,
+            you should return None.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        actions = self.getLegalActions(state)
+        actions_arr = []
+
+        if len(actions) == 0:
+            return None
+        
+        for action in actions:
+            actions_arr.append((self.getQValue(state, action), action))
+        
+        selected = [pair for pair in actions_arr if pair == max(actions_arr)]
+        rndm = random.choice(selected)
+
+        return rndm[1]
 
     def getAction(self, state):
         """
-          Compute the action to take in the current state.  With
-          probability self.epsilon, we should take a random action and
-          take the best policy action otherwise.  Note that if there are
-          no legal actions, which is the case at the terminal state, you
-          should choose None as the action.
+            Compute the action to take in the current state.  With
+            probability self.epsilon, we should take a random action and
+            take the best policy action otherwise.  Note that if there are
+            no legal actions, which is the case at the terminal state, you
+            should choose None as the action.
 
-          HINT: You might want to use util.flipCoin(prob)
-          HINT: To pick randomly from a list, use random.choice(list)
+            HINT: You might want to use util.flipCoin(prob)
+            HINT: To pick randomly from a list, use random.choice(list)
         """
         # Pick Action
         legalActions = self.getLegalActions(state)
-        action = None
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        
+        if util.flipCoin(p):
+            return random.choice(legalActions)
 
-        return action
+        return self.computeActionFromQValues(state)
 
     def update(self, state, action, nextState, reward):
         """
-          The parent class calls this to observe a
-          state = action => nextState and reward transition.
-          You should do your Q-Value update here
+            The parent class calls this to observe a
+            state = action => nextState and reward transition.
+            You should do your Q-Value update here
 
-          NOTE: You should never call this function,
-          it will be called on your behalf
+            NOTE: You should never call this function,
+            it will be called on your behalf
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        qSa = self.getQValue(state, action)
+
+        s = reward + self.discount*self.computeValueFromQValues(nextState)
+        self.qValues[(state, action)] = (1-self.alpha)*qSa + self.alpha*s
 
     def getPolicy(self, state):
         return self.computeActionFromQValues(state)
@@ -145,11 +167,11 @@ class PacmanQAgent(QLearningAgent):
 
 class ApproximateQAgent(PacmanQAgent):
     """
-       ApproximateQLearningAgent
+      ApproximateQLearningAgent
 
-       You should only have to overwrite getQValue
-       and update.  All other QLearningAgent functions
-       should work as is.
+      You should only have to overwrite getQValue
+      and update.  All other QLearningAgent functions
+      should work as is.
     """
     def __init__(self, extractor='IdentityExtractor', **args):
         self.featExtractor = util.lookup(extractor, globals())()
@@ -161,15 +183,15 @@ class ApproximateQAgent(PacmanQAgent):
 
     def getQValue(self, state, action):
         """
-          Should return Q(state,action) = w * featureVector
-          where * is the dotProduct operator
+            Should return Q(state,action) = w * featureVector
+            where * is the dotProduct operator
         """
         "*** YOUR CODE HERE ***"
         util.raiseNotDefined()
 
     def update(self, state, action, nextState, reward):
         """
-           Should update your weights based on transition
+            Should update your weights based on transition
         """
         "*** YOUR CODE HERE ***"
         util.raiseNotDefined()
